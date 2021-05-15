791a9b5fc22764f7a78a0221b472713ba112b9c9
“briana <briana@thesistesting.com>
2021-05-15 15:15:10 -0400

    create whack a mole game

--- app/js/global.js
+++ app/js/global.js
@@ -1,3 +1,50 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log('helloooooo')
-});
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+
+  lastHole = hole;
+  return hole;
+}
+
+function pop() {
+  const time = randomTime(300, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) pop();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  window.scrollTo(0,document.body.scrollHeight);
+  timeUp = false;
+  score = 0;
+  pop();
+  setTimeout(() => timeUp = true, 10000)
+}
+
+function whack(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.parentNode.classList.remove('up');
+  scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', whack));
