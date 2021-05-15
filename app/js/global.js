415c670e1e337f4664994d344c20de5c2bd0fbba
“briana <briana@thesistesting.com>
2021-05-15 19:38:55 -0400

    update mole selector

--- app/js/global.js
+++ app/js/global.js
@@ -1,6 +1,6 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
+const moles = document.querySelectorAll('.mole img');
 
 let lastHole;
 let timeUp = false;
