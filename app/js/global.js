d178b1a51bdf06025343c7a27644a7b62cc0acdd
“briana <briana@thesistesting.com>
2021-05-19 23:35:00 -0400

    add some functionality

--- app/js/global.js
+++ app/js/global.js
@@ -1,10 +1,38 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
+const mole = document.querySelectorAll('.mole');
 const moles = document.querySelectorAll('.mole img');
+const gameTime = document.querySelector('.timer');
+const difficultyBtn = document.querySelectorAll('.difficulty-wrapper button')
 
-let lastHole;
-let timeUp = false;
-let score = 0;
+let 
+  lastHole,
+  timeUp = false,
+  timeLeft,
+  timer,
+  minimum = 500,
+  maximum = 1200;
+
+function setDifficulty(min, max){
+  minimum = min
+  maximum = max
+}
+
+function startTimer() {
+  clearInterval(timer)
+  timeLeft = 10
+  gameTime.innerHTML = timeLeft
+  
+  timer = setInterval(function(){ 
+    timeLeft--
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+    }
+
+    gameTime.innerHTML = timeLeft
+  }, 1000);
+}
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -13,6 +41,7 @@ function randomTime(min, max) {
 function randomHole(holes) {
   const idx = Math.floor(Math.random() * holes.length);
   const hole = holes[idx];
+
   if (hole === lastHole) {
     return randomHole(holes);
   }
@@ -22,9 +51,10 @@ function randomHole(holes) {
 }
 
 function pop() {
-  const time = randomTime(300, 1000);
+  const time = randomTime(minimum, maximum);
   const hole = randomHole(holes);
   hole.classList.add('up');
+
   setTimeout(() => {
     hole.classList.remove('up');
     if (!timeUp) pop();
@@ -36,6 +66,7 @@ function startGame() {
   window.scrollTo(0,document.body.scrollHeight);
   timeUp = false;
   score = 0;
+  startTimer()
   pop();
   setTimeout(() => timeUp = true, 10000)
 }
@@ -44,7 +75,21 @@ function whack(e) {
   if (!e.isTrusted) return;
   score++;
   this.parentNode.classList.remove('up');
+  $(this).parent().addClass('whacked')
+
+  var that = this;
+  setTimeout(function() {
+    $(that).parent().removeClass('whacked')
+  }, 1000);
+
   scoreBoard.textContent = score;
 }
 
 moles.forEach(mole => mole.addEventListener('click', whack));
+
+$('img').on('dragstart', function(event) { event.preventDefault(); });
+
+$(difficultyBtn).click(function(){
+  $(this).addClass('selected').siblings().removeClass('selected')
+})
+
